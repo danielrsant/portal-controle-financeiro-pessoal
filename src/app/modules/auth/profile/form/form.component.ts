cd763c35c61a9e31b5d57fede98c32dc8d67d728
danielrsant <danrsant14@gmail.com>
2020-11-04 22:32:25 -0300

    profile

--- src/app/modules/auth/profile/form/form.component.ts
+++ src/app/modules/auth/profile/form/form.component.ts
@@ -2,9 +2,12 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import { CategoryService } from 'src/app/services/category.service';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { ProfileService } from 'src/app/services/profile.service';
 import { LoadingService } from 'src/app/shared/components/several-components/loading/loading.service';
 import { Operation } from 'src/app/shared/enums/operation';
+import { StyleService } from 'src/app/shared/services/style.service';
 
 @Component({
   selector: 'app-form',
@@ -21,18 +24,32 @@ export class FormComponent implements OnInit, OnDestroy {
   form: FormGroup;
   dtToday = new Date();
 
-  images = ['1', '2', '3', '4', '5', '6', '7', '8'];
-  imageSelected = '1';
+  images = ['assets/images/users/1.png', 
+    'assets/images/users/2.png', 
+    'assets/images/users/3.png', 
+    'assets/images/users/4.png', 
+    'assets/images/users/5.png', 
+    'assets/images/users/6.png', 
+    'assets/images/users/7.png', 
+    'assets/images/users/8.png'
+  ];
+  imageSelected$: Observable<string>;
+  imageSelected: string;
+
+  destroy$ = new Subject();
 
   constructor(
     private _router: Router,
     private _activatedRoute: ActivatedRoute,
-    private _categoryService: CategoryService,
+    private _profileService: ProfileService,
     private _loadingService: LoadingService,
+    private _styleService: StyleService,
     private _toastr: ToastrService
   ) { }
 
   ngOnInit(): void {
+    this.imageSelected$ = this._styleService.image$;
+    this.imageSelected$.pipe(takeUntil(this.destroy$)).subscribe(data => this.imageSelected = data);
     this.setOperation();
     this.createForm();
     this.setForm();
@@ -46,7 +63,6 @@ export class FormComponent implements OnInit, OnDestroy {
   createForm(): void {
     this.form = new FormGroup({
       id: new FormControl(null),
-      image: new FormControl(null),
       descricao: new FormControl(null, [
         Validators.required,
         Validators.maxLength(80),
@@ -61,9 +77,9 @@ export class FormComponent implements OnInit, OnDestroy {
 
   setForm(): void {
     this._loadingService.show();
-    this._categoryService.loadOne(1).subscribe(
+    this._profileService.loadUser().pipe(takeUntil(this.destroy$)).subscribe(
       (response: any) => {
-        this.form.patchValue(response);
+        this.form.patchValue(response.data[0]);
 
         if (this.operation === Operation.VIEW) {
           this.form.disable();
@@ -72,7 +88,7 @@ export class FormComponent implements OnInit, OnDestroy {
         this._loadingService.hide();
       },
       (err) => {
-        console.log(err);
+        this._toastr.error(err.error.message);
         this._loadingService.hide();
       }
     );
@@ -87,21 +103,24 @@ export class FormComponent implements OnInit, OnDestroy {
   }
 
   onUpdate(): void {
-    this._categoryService.update(this.form.value.id, this.form.value).subscribe(
-      (response: any) => {
-        this._loadingService.hide();
-        this._router.navigate(['../..'], { relativeTo: this._activatedRoute });
-      },
-      (error) => {
-        this._toastr.error(error.error.message);
-        this._loadingService.hide();
-      }
-    );
+    // this._profileService.update(this.form.value.id, this.form.value).subscribe(
+    //   (response: any) => {
+    //     this._loadingService.hide();
+    //     this._router.navigate(['../..'], { relativeTo: this._activatedRoute });
+    //   },
+    //   (error) => {
+    //     this._toastr.error(error.error.message);
+    //     this._loadingService.hide();
+    //   }
+    // );
   }
 
   selectImage(image): void {
-    this.imageSelected = image;
+    this._styleService.setImage(image);
   }
 
-  ngOnDestroy(): void { }
+  ngOnDestroy(): void { 
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
---
+++ src/app/services/profile.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+  API_BASE_URL: string = environment.API;
+  pessoaId = JSON.parse(localStorage.getItem('user')).id;
+
+  constructor(private http: HttpClient) { }
+
+  loadUser(payload?: any): Observable<any[]> {
+    const url = `${this.API_BASE_URL}/pessoas?filter=id||$eq||${this.pessoaId}`;
+
+    return this.http.get(url, { params: payload }).pipe(map((result: any) => result));
+  }
+
+  loadOne(id: number): Observable<any> {
+    const url = `${ this.API_BASE_URL }/usuario/${ id } `;
+
+    return this.http.get(url).pipe(map((result: any) => result));
+  }
+
+  save(payload: any): Observable<any> {
+    const url = `${ this.API_BASE_URL }/tarefas`;
+
+    return this.http.post(url, payload);
+  }
+
+  destroy(id: number): Observable<any> {
+    const url = `${ this.API_BASE_URL }/tarefas/${id}`;
+
+    return this.http.delete(url);
+  }
+
+}
